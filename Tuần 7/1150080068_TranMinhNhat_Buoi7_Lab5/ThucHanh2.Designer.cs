ae998c79a6594c94674d8c5501fed8d4b26d2e2d
Nhật <vvkghouls@gmail.com>
2025-10-13 08:50:21 +0700

    PPPTPMHDT tuần 7

---
+++ Tuần 7/1150080068_TranMinhNhat_Buoi7_Lab5/ThucHanh2.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace _1150080068_TranMinhNhat_Buoi7_Lab5
+{
+    partial class ThucHanh2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnThemSV = new System.Windows.Forms.Button();
+            this.cboGT = new System.Windows.Forms.ComboBox();
+            this.dtmNS = new System.Windows.Forms.DateTimePicker();
+            this.txtML = new System.Windows.Forms.TextBox();
+            this.txtQQ = new System.Windows.Forms.TextBox();
+            this.txtTSV = new System.Windows.Forms.TextBox();
+            this.txtMSV = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lstvBangSV = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnThemSV);
+            this.groupBox1.Controls.Add(this.cboGT);
+            this.groupBox1.Controls.Add(this.dtmNS);
+            this.groupBox1.Controls.Add(this.txtML);
+            this.groupBox1.Controls.Add(this.txtQQ);
+            this.groupBox1.Controls.Add(this.txtTSV);
+            this.groupBox1.Controls.Add(this.txtMSV);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(1, 104);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(383, 542);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Nhập thông tin";
+            // 
+            // btnThemSV
+            // 
+            this.btnThemSV.Location = new System.Drawing.Point(36, 436);
+            this.btnThemSV.Name = "btnThemSV";
+            this.btnThemSV.Size = new System.Drawing.Size(305, 52);
+            this.btnThemSV.TabIndex = 3;
+            this.btnThemSV.Text = "Thêm sinh viên";
+            this.btnThemSV.UseVisualStyleBackColor = true;
+            this.btnThemSV.Click += new System.EventHandler(this.btnThemSV_Click);
+            // 
+            // cboGT
+            // 
+            this.cboGT.FormattingEnabled = true;
+            this.cboGT.Location = new System.Drawing.Point(112, 175);
+            this.cboGT.Name = "cboGT";
+            this.cboGT.Size = new System.Drawing.Size(265, 28);
+            this.cboGT.TabIndex = 11;
+            this.cboGT.SelectedIndexChanged += new System.EventHandler(this.cboGT_SelectedIndexChanged);
+            // 
+            // dtmNS
+            // 
+            this.dtmNS.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtmNS.Location = new System.Drawing.Point(112, 223);
+            this.dtmNS.Name = "dtmNS";
+            this.dtmNS.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.dtmNS.Size = new System.Drawing.Size(265, 26);
+            this.dtmNS.TabIndex = 10;
+            this.dtmNS.ValueChanged += new System.EventHandler(this.dtmNS_ValueChanged);
+            // 
+            // txtML
+            // 
+            this.txtML.Location = new System.Drawing.Point(112, 335);
+            this.txtML.Name = "txtML";
+            this.txtML.Size = new System.Drawing.Size(265, 26);
+            this.txtML.TabIndex = 9;
+            this.txtML.TextChanged += new System.EventHandler(this.txtML_TextChanged);
+            // 
+            // txtQQ
+            // 
+            this.txtQQ.Location = new System.Drawing.Point(112, 279);
+            this.txtQQ.Name = "txtQQ";
+            this.txtQQ.Size = new System.Drawing.Size(265, 26);
+            this.txtQQ.TabIndex = 8;
+            this.txtQQ.TextChanged += new System.EventHandler(this.txtQQ_TextChanged);
+            // 
+            // txtTSV
+            // 
+            this.txtTSV.Location = new System.Drawing.Point(112, 122);
+            this.txtTSV.Name = "txtTSV";
+            this.txtTSV.Size = new System.Drawing.Size(265, 26);
+            this.txtTSV.TabIndex = 7;
+            this.txtTSV.TextChanged += new System.EventHandler(this.txtTSV_TextChanged);
+            // 
+            // txtMSV
+            // 
+            this.txtMSV.Location = new System.Drawing.Point(112, 65);
+            this.txtMSV.Name = "txtMSV";
+            this.txtMSV.Size = new System.Drawing.Size(265, 26);
+            this.txtMSV.TabIndex = 6;
+            this.txtMSV.TextChanged += new System.EventHandler(this.txtMSV_TextChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(10, 338);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(60, 20);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "Mã lớp:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(10, 282);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(83, 20);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Quê quán:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(10, 228);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 20);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Ngày sinh:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 178);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 20);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Giới tính:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 20);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Tên sinh viên:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Mã sinh viên:";
+            // 
+            // lstvBangSV
+            // 
+            this.lstvBangSV.HideSelection = false;
+            this.lstvBangSV.Location = new System.Drawing.Point(8, 25);
+            this.lstvBangSV.Name = "lstvBangSV";
+            this.lstvBangSV.Size = new System.Drawing.Size(919, 506);
+            this.lstvBangSV.TabIndex = 1;
+            this.lstvBangSV.UseCompatibleStateImageBehavior = false;
+            this.lstvBangSV.SelectedIndexChanged += new System.EventHandler(this.lstvBangSV_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(342, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(264, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Thêm dữ liệu không dùng parameter";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lstvBangSV);
+            this.groupBox2.Location = new System.Drawing.Point(390, 104);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(940, 542);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Danh sách sinh viên";
+            // 
+            // ThucHanh2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1334, 647);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "ThucHanh2";
+            this.Text = "Thêm dữ liệu không dùng parameter";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtML;
+        private System.Windows.Forms.TextBox txtQQ;
+        private System.Windows.Forms.TextBox txtTSV;
+        private System.Windows.Forms.TextBox txtMSV;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListView lstvBangSV;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnThemSV;
+        private System.Windows.Forms.ComboBox cboGT;
+        private System.Windows.Forms.DateTimePicker dtmNS;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
---
+++ Tuần 7/1150080068_TranMinhNhat_Buoi7_Lab5/ThucHanh2.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using Oracle.ManagedDataAccess.Client;
+
+namespace _1150080068_TranMinhNhat_Buoi7_Lab5
+{
+    public partial class ThucHanh2 : Form
+    {
+        // ===== CẤU HÌNH ORACLE =====
+        private const string ORACLE_CS =
+            "User Id=app;Password=app123;Data Source=localhost:1521/XEPDB1;";
+        private const string TABLE_NAME = "SINHVIEN2";
+
+        private OracleConnection _conn;
+
+        public ThucHanh2()
+        {
+            InitializeComponent();
+
+            // Chuẩn hóa ListView (nếu designer chưa cấu hình)
+            lstvBangSV.View = View.Details;
+            lstvBangSV.FullRowSelect = true;
+            lstvBangSV.GridLines = true;
+            if (lstvBangSV.Columns.Count == 0)
+            {
+                lstvBangSV.Columns.Add("Mã SV", 90);
+                lstvBangSV.Columns.Add("Tên SV", 120);
+                lstvBangSV.Columns.Add("Giới tính", 80);
+                lstvBangSV.Columns.Add("Ngày sinh", 100);
+                lstvBangSV.Columns.Add("Quê quán", 100);
+                lstvBangSV.Columns.Add("Mã lớp", 80);
+            }
+
+            // Giới tính
+            cboGT.DropDownStyle = ComboBoxStyle.DropDownList;
+            cboGT.Items.Clear();
+            cboGT.Items.Add("Nam");
+            cboGT.Items.Add("Nữ");
+            if (cboGT.Items.Count > 0) cboGT.SelectedIndex = 0;
+
+            // Validate để bật/tắt nút Thêm
+            txtMSV.TextChanged += (s, e) => ToggleAddButton();
+            txtTSV.TextChanged += (s, e) => ToggleAddButton();
+            ToggleAddButton();
+
+            // Kết nối & tải dữ liệu
+            try
+            {
+                _conn = new OracleConnection(ORACLE_CS);
+                _conn.Open();
+                HienThiDanhSach();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Không mở được kết nối Oracle.\n" + ex.Message,
+                    "Lỗi kết nối", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void ToggleAddButton()
+        {
+            btnThemSV.Enabled = !string.IsNullOrWhiteSpace(txtMSV.Text)
+                                && !string.IsNullOrWhiteSpace(txtTSV.Text);
+        }
+
+        // ============== HIỂN THỊ DANH SÁCH (SELECT) ==============
+        private void HienThiDanhSach()
+        {
+            if (_conn == null || _conn.State != ConnectionState.Open) return;
+
+            string sql = string.Format(
+                "SELECT MASV, TENSV, GIOITINH, NGAYSINH, QUEQUAN, MALOP FROM {0} ORDER BY MASV",
+                TABLE_NAME);
+
+            using (var cmd = new OracleCommand(sql, _conn))
+            {
+                lstvBangSV.Items.Clear();
+                using (var r = cmd.ExecuteReader())
+                {
+                    while (r.Read())
+                    {
+                        string maSv = r.GetString(0);
+                        string tenSv = r.GetString(1);
+                        string gt = r.GetString(2);
+                        string ngaySinh = r.GetDateTime(3).ToString("dd/MM/yyyy");
+                        string queQuan = r.IsDBNull(4) ? "" : r.GetString(4);
+                        string maLop = r.IsDBNull(5) ? "" : r.GetString(5);
+
+                        var item = new ListViewItem(maSv);
+                        item.SubItems.Add(tenSv);
+                        item.SubItems.Add(gt);
+                        item.SubItems.Add(ngaySinh);
+                        item.SubItems.Add(queQuan);
+                        item.SubItems.Add(maLop);
+                        lstvBangSV.Items.Add(item);
+                    }
+                }
+            }
+        }
+
+        // ============== THÊM (INSERT) — KHÔNG DÙNG PARAMETER ==============
+        private void btnThemSV_Click(object sender, EventArgs e)
+        {
+            if (_conn == null || _conn.State != ConnectionState.Open)
+            {
+                MessageBox.Show("Chưa kết nối Oracle.");
+                return;
+            }
+
+            try
+            {
+                string maSV = txtMSV.Text.Trim();
+                string tenSV = txtTSV.Text.Trim();
+                string gioiTinh = (cboGT.SelectedItem ?? "").ToString();
+                string ngayIso = dtmNS.Value.ToString("yyyy-MM-dd");
+                string queQuan = txtQQ.Text.Trim();
+                string maLop = txtML.Text.Trim();
+
+                if (string.IsNullOrEmpty(maSV) || string.IsNullOrEmpty(tenSV))
+                {
+                    MessageBox.Show("Mã SV và Tên SV bắt buộc nhập.");
+                    return;
+                }
+
+                string sql = string.Format(
+                    "INSERT INTO {0} (MASV,TENSV,GIOITINH,NGAYSINH,QUEQUAN,MALOP) " +
+                    "VALUES ('{1}', '{2}', '{3}', TO_DATE('{4}','YYYY-MM-DD'), '{5}', '{6}')",
+                    TABLE_NAME, maSV, tenSV, gioiTinh, ngayIso, queQuan, maLop);
+
+                using (var cmd = new OracleCommand(sql, _conn))
+                {
+                    int kq = cmd.ExecuteNonQuery();
+
+                    using (var commit = new OracleCommand("COMMIT", _conn))
+                    {
+                        commit.ExecuteNonQuery();
+                    }
+
+                    if (kq > 0)
+                    {
+                        MessageBox.Show("Thêm sinh viên thành công!");
+                        HienThiDanhSach();
+                        ClearInputs();
+                    }
+                }
+            }
+            catch (OracleException ox)
+            {
+                if (ox.Number == 1)
+                    MessageBox.Show("Mã SV đã tồn tại (trùng khóa chính).", "Lỗi",
+                        MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                else
+                    MessageBox.Show("Lỗi Oracle: " + ox.Message, "Lỗi",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Thêm dữ liệu bị lỗi: " + ex.Message, "Lỗi",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void ClearInputs()
+        {
+            txtMSV.Clear();
+            txtTSV.Clear();
+            cboGT.SelectedIndex = 0;
+            dtmNS.Value = DateTime.Today;
+            txtQQ.Clear();
+            txtML.Clear();
+            txtMSV.Focus();
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            try
+            {
+                if (_conn != null && _conn.State == ConnectionState.Open)
+                    _conn.Close();
+            }
+            catch { }
+            base.OnFormClosing(e);
+        }
+
+        // Handlers trống
+        private void txtMSV_TextChanged(object sender, EventArgs e) { }
+        private void txtTSV_TextChanged(object sender, EventArgs e) { }
+        private void cboGT_SelectedIndexChanged(object sender, EventArgs e) { }
+        private void dtmNS_ValueChanged(object sender, EventArgs e) { }
+        private void txtQQ_TextChanged(object sender, EventArgs e) { }
+        private void txtML_TextChanged(object sender, EventArgs e) { }
+        private void lstvBangSV_SelectedIndexChanged(object sender, EventArgs e) { }
+    }
+}
